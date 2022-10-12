e82f75a516d84f08fab78fb3e908ce6c09f04e9b
allanvictormm <allanvictormm@gmail.com>
2022-10-12 14:43:35 -0300

    Estudando anatomia de classes

--- Testes e exercícios/anatomia-de-classes/src/MinhaClasse.java
+++ Testes e exercícios/anatomia-de-classes/src/MinhaClasse.java
@@ -1,8 +1,15 @@
 public class MinhaClasse {
 
 public static void main (String [] args) {
-    System.out.print ("Meu primeiro código Java");
+    String primeiroNome = "Allan";
+    String segundoNome = "Victor";
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
 
 }
-    
+ public static String nomeCompleto (String primeiroNome,String segundoNome) {
+    return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+
+ }  
+
 }
