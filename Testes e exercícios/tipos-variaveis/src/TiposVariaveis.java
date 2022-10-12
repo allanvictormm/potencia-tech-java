d3a39b7a5d1acfe8c801bb58881201916a30f032
allanvictormm <allanvictormm@gmail.com>
2022-10-12 19:32:45 -0300

    Estudando tipos e variaveis

--- Testes e exercícios/tipos-variaveis/src/TiposVariaveis.java
+++ Testes e exercícios/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
     }
 }
